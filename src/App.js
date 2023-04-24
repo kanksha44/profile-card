5885e6af12874d8388415fc050f1a70d81a918e6
kanksha4496 <kanksha44@gmail.com>
2023-04-25 02:46:44 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Profilecard from "./component/profile.card/Profilecard";
 
 function App() {
+  const users = [
+    {
+      name: "Raju Shrivastava",
+      designation: "SOFTWARE ENGINEER",
+      jobDescription:
+        "Writes, tests, and maintains software code, collaborating with cross-functional teams.",
+      image: "https://picsum.photos/id/64/200/300",
+    },
+    {
+      name: "Aakanksha Gaikwad",
+      designation: "FRONT-END ENGINEER",
+      jobDescription:
+        "Develops user-facing features using HTML, CSS, and JavaScript.",
+      image: "https://picsum.photos/id/65/200/300",
+    },
+    {
+      name: "John leo",
+      designation: "SENIOR SOFTWARE ENGINEER",
+      jobDescription:
+        "Leads development projects, designs software architecture, and mentors junior developers.",
+      image: "https://picsum.photos/id/61/200/300",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user, index) => (
+        <Profilecard
+          key={index}
+          image={user.image}
+          designation={user.designation}
+          name={user.name}
+          jobdesc={user.jobDescription}
+          onSeeMoreClick={() => alert(user.name)}
+        />
+      ))}
     </div>
   );
 }
